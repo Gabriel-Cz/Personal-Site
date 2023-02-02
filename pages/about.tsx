dbd7af5624d8a8ad3929a1b8957642f64d666946
Gabriel-Cz <gabrielczhz@gmail.com>
2023-02-01 23:35:34 -0600

    FUNC (dev) Dynamic fetch url

--- pages/about.tsx
+++ pages/about.tsx
@@ -3,7 +3,7 @@ import { Content, Text, Button, Layout } from "@components";
 import { PageProps } from "@types";
 
 export const getServerSideProps = async () => {
-  const res = await fetch('http://localhost:3000/api/aws?pageName=Experience', {
+  const res = await fetch(`${process.env.REACT_URL}/api/aws?pageName=Experience`, {
     method: 'GET',
     headers: {
       "Content-type": "application/json",
--- pages/techs.tsx
+++ pages/techs.tsx
@@ -3,7 +3,7 @@ import { Content, Techs, Button, Layout } from "@components";
 import { PageProps } from "@types";
 
 export const getServerSideProps = async () => {
-  const res = await fetch('http://localhost:3000/api/aws?pageName=Techs', {
+  const res = await fetch(`${process.env.REACT_URL}/api/aws?pageName=Techs`, {
     method: 'GET',
     headers: {
       "Content-type": "application/json",
