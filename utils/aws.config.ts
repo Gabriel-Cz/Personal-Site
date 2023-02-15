da97f9988d080e858d43f4407eed0495a2dc0b61
Gabriel-Cz <gabrielczhz@gmail.com>
2023-02-14 22:45:23 -0600

    FUNC (dev) Update aws service

--- utils/aws.config.ts
+++ utils/aws.config.ts
@@ -3,10 +3,9 @@ import AWS from 'aws-sdk';
 AWS.config.update({
   accessKeyId: process.env.DB_ACCESS_KEY_ID,
   secretAccessKey: process.env.DB_SECRET_ACCESS_KEY,
-  region: 'us-west-1'
+  region: 'us-east-1'
 });
 
 const db = new AWS.DynamoDB.DocumentClient({ apiVersion: 'latest' });
-const ses = new AWS.SES({ apiVersion: 'latest' });
 
-export { db, ses };
+export { db };
