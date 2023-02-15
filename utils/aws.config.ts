fb1cd89c0eaa4b594b7aa825fcf086cfc6639b32
Gabriel-Cz <gabrielczhz@gmail.com>
2023-02-14 20:26:51 -0600

    FUNC (dev) Update aws region

--- utils/aws.config.ts
+++ utils/aws.config.ts
@@ -3,7 +3,7 @@ import AWS from 'aws-sdk';
 AWS.config.update({
   accessKeyId: process.env.DB_ACCESS_KEY_ID,
   secretAccessKey: process.env.DB_SECRET_ACCESS_KEY,
-  region: 'us-east-1'
+  region: 'us-west-1'
 });
 
 const db = new AWS.DynamoDB.DocumentClient({ apiVersion: 'latest' });
